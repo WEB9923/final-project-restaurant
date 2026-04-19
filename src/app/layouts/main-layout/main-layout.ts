159e664586fd62818e84ecee3a6cc2d8d2d59c07
amiko <amikochixladze1234@gmail.com>
2026-04-19 14:15:19 +0400

    Almost finishing footer

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../../components/shared/navbar/navbar';
+import { Footer } from '../../components/shared/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   template: `
     <app-navbar></app-navbar>
-    <main>
+    <main class="min-h-[calc(100vh-70px)] mb-8">
       <router-outlet></router-outlet>
     </main>
+    <app-footer></app-footer>
   `,
 })
 export class MainLayout {}
